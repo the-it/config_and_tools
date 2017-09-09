0cbdb68765277d5ed323d11a389f05936db5b4cf
Erik Sommer <erotech@posteo.de>
2017-09-09 14:03:29 +0200

    make the Renamer nice - get rid of Command line arguments - PEP8 compliant - tidy up

--- random_renamer/random_renamer.py
+++ random_renamer/random_renamer.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
-import os, sys
+import os
 import random
 import string
+import sys
 
-print(sys.path)
-print(os.getcwd())
 
 class RandomRenamer(object):
     def __init__(self):
-        if len(sys.argv) == 2:
-            self.work_dir = sys.argv[1]
-        else:
-            self.work_dir = os.getcwd()
-        if len(sys.argv) > 2:
-            raise TypeError('To much command line arguments.')
+        self.work_dir = os.getcwd()
+        if sys.argv > 2:
+            raise TypeError('No command line arguments allowed.')
 
     @staticmethod
     def _get_random_string():
-        return ''.join(random.sample(string.ascii_lowercase, random.randint(5,10)))
+        return ''.join(random.sample(string.ascii_lowercase, random.randint(5, 10)))
 
     def run(self):
-        for root, dirs, files in os.walk(self.work_dir, topdown=False):
+        for root, directories, files in os.walk(self.work_dir, topdown=False):
             print('root: {}'.format(root))
-            print('dirs: {}'.format(dirs))
+            print('dirs: {}'.format(directories))
             print('files: {}'.format(files))
             for file in files:
                 if file in __file__:
@@ -31,8 +27,8 @@ class RandomRenamer(object):
                 file_split[0] = self._get_random_string()
                 new_file = '.'.join(file_split)
                 os.rename('/'.join((root, file)), '/'.join((root, new_file)))
-            for dir in dirs:
-                os.rename('/'.join((root, dir)), '/'.join((root, self._get_random_string())))
+            for directory in directories:
+                os.rename('/'.join((root, directory)), '/'.join((root, self._get_random_string())))
 
 if '__main__' == __name__:
     RandomRenamer().run()
